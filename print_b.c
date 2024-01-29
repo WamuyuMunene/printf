52a14bd2e4d2597678689d62d1b87008aded1d9e
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-29 09:40:48 -0800

    binary rep

--- print_b.c
+++ print_b.c
@@ -12,21 +12,20 @@ int print_b(va_list value)
 {
 	int flag = 0;
 	int counter = 0;
-	int i, a = 1, b;
+	int i;
 	unsigned int num = va_arg(value, unsigned int);
 	unsigned int temp;
 
 	for (i = 0; i < 32; i++)
 	{
-		temp = ((a << (32 - i)) & num);
+		temp = ((1u << (32 - i)) & num);
 		if (temp >> (31 - i))
 		{
 			flag = 1;
 		}
 		if (flag)
 		{
-			b = temp >> (31 - i);
-			_putchar(b + '0');
+			_putchar((temp >> (31 - i)) + '0');
 			counter++;
 		}
 	}
