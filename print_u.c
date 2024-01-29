080b02f08ff3eea3d3244adea3ab9dd10dea9655
WamuyuMunene <wamuyumunene98@gmail.com>
2024-01-29 11:05:39 -0800

    unsigned int

--- print_u.c
+++ print_u.c
@@ -10,18 +10,40 @@
 
 int print_u(va_list value)
 {
-	int i = 0;
-	unsigned int num;
+	unsigned int n = va_arg(value, unsigned int);
+	int num;
+	int last = n % 10;
 	int digit;
+	int exp = 1;
+	int i = 1;
 
-	num = va_arg(value, unsigned int);
-
-	do {
+	n = n / 10;
+	num = n;
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
 		i++;
-		digit = num % 10;
-		_putchar(digit + '0');
-		num = num / 10;
-	} while (num > 0);
-
-	return (i - 1);
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+	return (i);
 }
